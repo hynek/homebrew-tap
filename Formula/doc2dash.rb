95c057ec4b4c40f2b18abab93edce8c099eb8a33
Hynek Schlawack <hs@ox.cx>
2022-09-14 11:21:32 +0200

    Add formula

---
+++ Formula/doc2dash.rb
@@ -0,0 +1,58 @@
+class Doc2dash < Formula
+  include Language::Python::Virtualenv
+
+  desc "Create docsets for Dash.app-compatible API browsers."
+  homepage "https://doc2dash.rtfd.io/"
+  url "https://files.pythonhosted.org/packages/c0/4c/e52c98d19c6abc179a682a5a5e4877551df12dca30335e6678d6cd555a76/doc2dash-3.0.0.tar.gz"
+  sha256 "5456ee60cce489dd03f6e236b2d997f949d17de3a0fce5e6be3c262493efa1ee"
+  head "https://github.com/hynek/doc2dash.git", branch: "main"
+  license "MIT"
+
+  depends_on "python@3.10"
+
+  resource "attrs" do
+    url "https://files.pythonhosted.org/packages/1a/cb/c4ffeb41e7137b23755a45e1bfec9cbb76ecf51874c6f1d113984ecaa32c/attrs-22.1.0.tar.gz"
+    sha256 "29adc2665447e5191d0e7c568fde78b21f9672d344281d0c6e1ab085429b22b6"
+  end
+
+  resource "beautifulsoup4" do
+    url "https://files.pythonhosted.org/packages/e8/b0/cd2b968000577ec5ce6c741a54d846dfa402372369b8b6861720aa9ecea7/beautifulsoup4-4.11.1.tar.gz"
+    sha256 "ad9aa55b65ef2808eb405f46cf74df7fcb7044d5cbc26487f96eb2ef2e436693"
+  end
+
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
+    sha256 "7682dc8afb30297001674575ea00d1814d808d6a36af415a82bd481d37ba7b8e"
+  end
+
+  resource "commonmark" do
+    url "https://files.pythonhosted.org/packages/60/48/a60f593447e8f0894ebb7f6e6c1f25dafc5e89c5879fdc9360ae93ff83f0/commonmark-0.9.1.tar.gz"
+    sha256 "452f9dc859be7f06631ddcb328b6919c67984aca654e5fefb3914d54691aed60"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/e0/ef/5905cd3642f2337d44143529c941cc3a02e5af16f0f65f81cbef7af452bb/Pygments-2.13.0.tar.gz"
+    sha256 "56a8508ae95f98e2b9bdf93a6be5ae3f7d8af858b43e02c5a2ff083726be40c1"
+  end
+
+  resource "rich" do
+    url "https://files.pythonhosted.org/packages/bb/2d/c902484141330ded63c6c40d66a9725f8da5e818770f67241cf429eef825/rich-12.5.1.tar.gz"
+    sha256 "63a5c5ce3673d3d5fbbf23cd87e11ab84b6b451436f1b7f19ec54b6bc36ed7ca"
+  end
+
+  resource "soupsieve" do
+    url "https://files.pythonhosted.org/packages/f3/03/bac179d539362319b4779a00764e95f7542f4920084163db6b0fd4742d38/soupsieve-2.3.2.post1.tar.gz"
+    sha256 "fc53893b3da2c33de295667a0e19f078c14bf86544af307354de5fcf12a3f30d"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python3.10")
+    venv.pip_install resources
+
+    venv.pip_install_and_link buildpath
+  end
+
+  test do
+    system "doc2dash --version"
+  end
+end
